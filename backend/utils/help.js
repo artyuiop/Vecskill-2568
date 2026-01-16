884abe5ff9e49b5a9ba9dc67432171a0f08d67fb
Thaparkon <thapakorn1365@gmail.com>
2026-01-16 20:59:37 +0700

    del All

--- backend/utils/help.js
+++
@@ -1,4 +0,0 @@
-const send = (res , data , code = 200) => res.status(code).json(data)
-const err = (res, e , msg = 'Server Error') => (console.log(e), send(res, {msg}, 500))
-
-module.exports = {send , err}
